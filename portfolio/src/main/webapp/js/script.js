f10c2705c687affd56d6d926db27254854afe66b
Arjan Singh Bal <arjan3141@gmail.com>
2020-07-08 14:53:52 +0530

    fetch name to display on carousel from servlet

--- portfolio/src/main/webapp/js/script.js
+++ portfolio/src/main/webapp/js/script.js
@@ -37,3 +37,9 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+async function getName() {
+  const response = await fetch('/data');
+  const name = await response.text();
+  document.getElementById('name-container').innerText = name;
+}
